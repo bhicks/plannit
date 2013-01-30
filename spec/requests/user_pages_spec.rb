d21d386a6a6ace22f3c7379f4e069ecbf03d4bb8
Ben Hicks <benkhicks@gmail.com>
2013-01-30 12:53:26 -0600

    Adding user requests tests

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -5,8 +5,6 @@ describe 'User pages' do
   subject { page }
 
   describe 'index' do
-    let(:user) { FactoryGirl.create(:user) }
-
     before(:each) do
       visit root_path
     end
@@ -17,4 +15,66 @@ describe 'User pages' do
     it { should have_link('Sign Up', href: new_user_registration_path ) }
   end
 
+  describe 'sign in' do
+    let(:user) { FactoryGirl.create(:user) }
+
+    before do 
+      sign_in user
+    end
+
+    describe 'my projects' do
+      it { should have_selector('h3', text: 'My Projects') }
+    end
+
+    describe 'edit profile' do
+      it { should have_link('Edit Profile', href: edit_user_registration_path) }
+
+      describe 'page' do
+        before { click_link 'Edit Profile' }
+
+        it { should have_content('Edit User') }
+        it { should have_content(user.email) }
+
+        describe 'password change' do
+          let(:new_password) { user.password + user.password }
+          let(:original_password) { user.encrypted_password }
+
+          before do
+            fill_in 'user_password', with: new_password
+            fill_in 'user_password_confirmation', with: new_password
+            fill_in 'user_current_password', with: user.password
+          end
+
+          after { user.password = new_password }
+
+          it 'should change the password' do
+            click_button 'Update'
+            user_model = User.find(user.id)
+            user_model.encrypted_password.should_not == original_password
+          end
+        end
+      end
+    end
+
+    describe 'logging out' do
+      it { should have_link('Logout', href: destroy_user_session_path) }
+
+      describe 'click loggout button' do
+        before { click_link 'Logout' }
+        it { should have_link('Login', href: new_user_session_path) }
+      end
+
+      describe 'logging back in' do
+        it { should have_content(user.email) }
+      end
+
+      describe 'logged in as' do
+        it { should have_content("Logged in as #{user.email}") }
+      end
+
+      describe 'start a new project' do
+        it { should have_link('Start a New Project', href: '#new_project_modal') }
+      end
+    end
+  end
 end
